479398ea179756790a7e4f7e81fdc4fb87b994fb
Gravy Gatsby <mcshaned@umich.edu>
2025-09-27 10:42:06 -0500

    camera rotation logic

--- Assets/Course Library/Scripts/RotateCamera.cs
+++ Assets/Course Library/Scripts/RotateCamera.cs
@@ -2,15 +2,10 @@ using UnityEngine;
 
 public class RotateCamera : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    public float rotationSpeed;
     void Update()
     {
-        
+        float horizontalInput = Input.GetAxis("Horizontal");
+        transform.Rotate(Vector3.up, horizontalInput * rotationSpeed * Time.deltaTime);
     }
 }
